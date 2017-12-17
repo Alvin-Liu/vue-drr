5c40f4b96b2ff7bbcc59abcc5ac826eb68613e51
alvin-liu <642451695@qq.com>
2017-12-17 14:21:08 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div style="height: 500px; width: 500px; margin: 20px; border: 1px solid red; position: relative;">
+      <vue-drr :x="50" :y="50" :angle="30" :w="100" :h="100" :parent="true">
+        <p>Component</p>
+      </vue-drr>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueDrr from './components/vue-drr'
+
+export default {
+  name: 'app',
+  components: {
+    'vue-drr': VueDrr
+  }
+}
+</script>
