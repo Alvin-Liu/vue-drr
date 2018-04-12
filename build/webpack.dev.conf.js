3b01a9cfaed8b52de78d6359e1780e961f8058d8
alvin <642451695@qq.com>
2018-04-13 00:15:23 +0800

    fixed dev bug

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,6 +2,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
 module.exports = {
   entry: './src/main.js',
